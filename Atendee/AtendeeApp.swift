151f44a228debbc92a855942c2932489abf62e6a
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-04-03 15:53:39 +0500

    Initial Commit

---
+++ Atendee/AtendeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtendeeApp.swift
+//  Atendee
+//
+//  Created by Muhammad Farid Ullah on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AtendeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
